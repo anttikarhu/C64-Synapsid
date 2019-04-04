c9798cad296315bb71f986ac9c08863462414839
Antti Karhu <antti.karhu@solita.fi>
2019-04-04 09:18:22 +0300

    You can now play an octave of notes using keys from Z to ,

--- app.asm
+++ app.asm
@@ -11,7 +11,10 @@ IRQADDRLSB      = $0315
 IRQCTRL         = $D01A
 IRQFLAG         = $D019
 IRQFINISH       = $EA31
-PORTB           = $DC01
+PORTREGA        = $DC00
+PORTREGB        = $DC01
+DATADIRA        = $DC02
+DATADIRB        = $DC03
 
 F_LSB1          = $D400
 F_MSB1          = $D401
@@ -20,8 +23,6 @@ ATT_DEC1        = $D405
 SUS_REL1        = $D406
 VFMODE          = $D418
 
-; TODO THIS IS JUST COPIED FROM C64-SID-PROGRAMMING REPO, THERE SHOULD BE
-; SOME STUFF IN COMMON HERE TOO
 
 INIT    LDA #%01111111 ; SWITCH OFF CIA-1 INTERRUPTS
         STA CIA1IRQ
@@ -39,63 +40,192 @@ INIT    LDA #%01111111 ; SWITCH OFF CIA-1 INTERRUPTS
         LDA #%00000001 ; RE-ENABLE RASTER INTERRUPTS ONLY AFTER SETUP
         STA IRQCTRL
 
-        ; SETTING UP VOICE 1
-        ; AFTER THESE STEPS, A SOUND CAN BE HEARD
-
         ; INITIALIZE FREQUENCIES. NOTE "A-3" ACCORDING TO http://sta.c64.org/cbm64sndfreq.html
         LDA #$8F
         STA F_LSB1
         LDA #$0E
         STA F_MSB1
 
-        ; SET SAW TOOTH VOICE (BIT 5) AND OPEN GATE (BIT 0)
-        LDA #%00100001
+        ; SETUP VOICE 1
+        ; SAW TOOTH (BIT 5), CLOSED GATE (BIT 0)
+        LDA #%00100000
         STA CTRL1
-
-        ; SET A SLOW ATTACK (7-4) AND A DECAY (3-0)
-        LDA #%11110000
+        ; FAST ATTACH (BITS 7-4) & DECAY (BITS 3-0)
+        LDA #%00000000
         STA ATT_DEC1
-
-        ; SET SUSTAIN (7-4) AND RELEASE (3-0), SOME VALUES
-        LDA #%11001000
+        ; LONG SUSTAIN (BITS 7-4) AND FAST RLEASE (BITS 3-0)
+        LDA #%11110000
         STA SUS_REL1
-
-        ; SET FILTERS (7-4) AND VOLUME TO 11 (3-0)
+        ; NO FILTERS (BITS 7-4) AND FULL VOLUME (BITS 3-0)
         LDA #%00001111
         STA VFMODE
 
-
         ; TODO:
-        ; STEP 1:
-        ; WAIT FOR KEYPRESS "Z" WHICH WILL BE A "C" NOTE
-        ; SET FREQUENCY FOR THE NOTE
-        ; OPEN GATE
-        ; WAIT FOR NO KEY INPUT
-        ; CLOSE GATE
-
-
-        ; WAIT UNTIL STOP KEY PRESSED THEN EXIT
+        ; EXPLAIN TO MYSELF HOW KEYBOARD READ WORKS
         ; TODO:
-        ; EXPLAIN TO MYSELF WHAT HAPPENS HERE
-        SEI
-        LDA #%11111111
-        STA $DC02             
+        ; NOW KEYS ARE CHECKED AND NOTES ARE UPDATED CONSTANTLY, 
+        ; MAYBE SHOULD DETECT 'KEYPRESS EVENTS' INSTEAD AND CHANGE NOTES
+        ; ONLY WHEN THE KEYPRESSES CHANGE? THIS WAY I QUESS THE ADSR STUFF
+        ; SHOULD WORK BETTER THAN NOW.    
+LOOP    LDA #%11111111
+        STA DATADIRA           
         LDA #%00000000
-        STA $DC03             
-        LDA #%01111111
-        STA $DC00
-WAITKEY LDA $DC01
-        AND #%10000000 
-        BNE WAITKEY
-        CLI
+        STA DATADIRB   
 
-CLEAN   LDA #0 ; CLEAR SID VOLUME AND FILTERS,
+CHK_C   ; DETECT KEY 'Z' DOWN
+        LDA #%11111101
+        STA PORTREGA
+        LDA PORTREGB
+        AND #%00010000 
+        BEQ NOTE_C
+
+CHK_D   ; 'X'
+        LDA #%11111011
+        STA PORTREGA
+        LDA PORTREGB
+        AND #%10000000 
+        BEQ NOTE_D
+
+CHK_E   ; 'C'
+        LDA #%11111011
+        STA PORTREGA
+        LDA PORTREGB
+        AND #%00010000 
+        BEQ NOTE_E
+
+CHK_F   ; 'V'
+        LDA #%11110111
+        STA PORTREGA
+        LDA PORTREGB
+        AND #%10000000 
+        BEQ NOTE_F
+
+CHK_G   ; 'B'
+        LDA #%11110111
+        STA PORTREGA
+        LDA PORTREGB
+        AND #%00010000 
+        BEQ NOTE_G
+
+CHK_A   ; 'N'
+        LDA #%11101111
+        STA PORTREGA
+        LDA PORTREGB
+        AND #%10000000 
+        BEQ NOTE_A
+
+CHK_B   ; 'M'
+        LDA #%11101111
+        STA PORTREGA
+        LDA PORTREGB
+        AND #%00010000 
+        BEQ NOTE_B
+
+CHK_C2  ; ','
+        LDA #%11011111
+        STA PORTREGA
+        LDA PORTREGB
+        AND #%10000000 
+        BEQ NOTE_C2
+
+CHK_Q   ; 'STOP'
+        LDA #%01111111
+        STA PORTREGA
+        LDA PORTREGB
+        AND #%10000000 
+        BEQ QUIT
+
+NO_NOTE ; NO KEYS PRESSED
+        LDA #%00100000
+        STA CTRL1
+        JMP LOOP
+
+NOTE_C  ; SET FREQUENCEY FOR 'C-3' AND OPEN GATE
+        LDX #$A8
+        LDY #$08
+        JSR PLAYNOTE
+        JMP LOOP
+
+NOTE_D  
+        LDX #$B7
+        LDY #$09
+        JSR PLAYNOTE
+        JMP LOOP
+
+NOTE_E
+        LDX #$E8
+        LDY #$0A
+        JSR PLAYNOTE
+        JMP LOOP
+
+NOTE_F
+        LDX #$8E
+        LDY #$0B
+        JSR PLAYNOTE
+        JMP LOOP
+
+NOTE_G
+        LDX #$F8
+        LDY #$0C
+        JSR PLAYNOTE
+        JMP LOOP
+
+NOTE_A
+        LDX #$8F
+        LDY #$0E
+        JSR PLAYNOTE
+        JMP LOOP
+
+NOTE_B
+        LDX #$57
+        LDY #$10
+        JSR PLAYNOTE
+        JMP LOOP
+
+NOTE_C2
+        LDX #$50
+        LDY #$11
+        JSR PLAYNOTE
+        JMP LOOP
+
+QUIT    LDA #0 ; CLEAR SID VOLUME AND FILTERS,
         STA VFMODE
         LDA #0 ; CLEAR VOICE 1 CONTROL,
         STA CTRL1
         RTS ; AND THEN QUIT
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+PLAYNOTE
+        ; SUBROUTINE THAT SETS A FREQUENCY AND OPENS GATE FOR VOICE 1
+        ; X REGISTER = FREQ LSB
+        ; Y REGISTER = FREQ MSB
+        STX X_ST ; STORE X AND Y TEMPORARILY
+        STY Y_ST
+        PHA ; PUSH A, X AND Y TO STACK TO BE A GOOD CITIZEN,
+        TXA
+        PHA
+        TYA
+        PHA
+        LDX X_ST ; AND GET X AND Y BACK FROM MEMORY
+        LDY Y_ST
+     
+        STX F_LSB1
+        STA F_MSB1
+        LDA #%00100001
+        STA CTRL1
+
+        PLA ; GET Y, X AND A BACK FROM THE STACK,
+        TAY
+        PLA
+        TAX
+        PLA
+        RTS ; AND END THE SUBROUTINE.
+X_ST    BYTE $00
+Y_ST    BYTE $00
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 PLAYIRQ 
         ASL IRQFLAG ; RESET IRQ FLAG
         JMP IRQFINISH ; LET MACHINE HANDLE OTHER IRQS
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
